c1842c683043bd248c88cd59cf091fc81c030b5a
Krsma <vlada.krsmanovic@gmail.com>
2018-05-07 18:04:56 +0200

    initial commit, added the adding of the fencers into a list and array

---
+++ Fencing app/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Fencing_app
+{
+   
+    public partial class Form1 : Form
+    {
+        List<fencer> competitors = new List<fencer>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string weapon = listBox1.GetItemText(listBox1.SelectedItem);
+            string categ = listBox2.GetItemText(listBox2.SelectedItem);
+            fencer competitor = new fencer(textBox1.Text, textBox2.Text, weapon, categ);
+            listBox3.Items.Add(competitor.ToString());
+            competitors.Add(competitor);
+                
+        }
+    }
+}
---
+++ Fencing app/fencer.cs
@@ -0,0 +1,24 @@
+﻿namespace Fencing_app
+{
+    internal class fencer
+    {
+        public string name;
+        public string club_name;
+        public string weapon;
+        public string category;
+        public int score = 0;
+        public int ranking = 0;
+
+        public fencer(string name, string club, string weapon, string category)
+        {
+            this.name = name;
+            this.club_name = name;
+            this.weapon = weapon;
+            this.category = category;
+        }
+        public override string ToString()
+        {
+            return "Fencer: " + name + " " + weapon + " " + category;
+        }
+    }
+}
